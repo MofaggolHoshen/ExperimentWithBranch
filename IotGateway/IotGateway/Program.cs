16cb7ada39c5210f22f92190d4dfbab5f480d799
Mofaggol Hoshen <summoncse@gmail.com>
2017-11-08 11:52:28 +0100

    Initial comments

---
+++ IotGateway/IotGateway/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IotGateway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is IotGateway branch!");
+            Console.Read();
+        }
+    }
+}
